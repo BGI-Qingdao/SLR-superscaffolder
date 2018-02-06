ea6eeae345bcbf16c7c9e63f422cbd2400356a9f
guolidong <guolidong@genomics.cn>
2018-02-06 14:43:01 +0800

    add build.sh

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+cd src ; make ; cd -;
+
+mv ./src/BarcodeOnContig ./bin/
+mv ./src/BarcodeOnBin ./bin/
+mv ./src/ClusterGap ./bin/
+mv ./src/FormatBarcodeOnRef ./bin/
+mv ./src/UnicomGraph ./bin/
+mv ./src/BinCluster ./bin/
+mv ./src/ContigTypeByRef ./bin/
+mv ./src/StaticsticUnique ./bin/
+mv ./src/LinearClusterResult ./bin/
+mv ./src/Sam2ReadInContig ./bin/
+mv ./src/test ./bin/
+mv ./src/ContigGraphType ./bin/
