82547b7b47c84d27c5000a592b94549e6e28f11a
guolidong <guolidong@genomics.cn>
2018-02-06 11:30:37 +0800

    job03->barcodeOnBin

--- src/BarcodeOnBin.cpp
+++ src/BarcodeOnBin.cpp
@@ -7,12 +7,12 @@ using namespace BGIQD::ARGS;
 
 int main(int argc ,char **argv)
 {
-    initLog("JOB03");
+    initLog("BarcodeOnBin");
 
     START_PARSE_ARGS
-    DEFINE_ARG(std::string , input , 'i');
-    DEFINE_ARG(std::string , output, 'o');
-    DEFINE_ARG(int , binsize, 'b');
+    DEFINE_ARG_DETAIL(std::string , input , 'i',false,"barcodeOnContig");
+    DEFINE_ARG_DETAIL(std::string , output, 'o',false,"output file");
+    DEFINE_ARG_DETAIL(int , binsize, 'b',false, "bin size");
     END_PARSE_ARGS
 
     contigBarcodeInfo cbi;
