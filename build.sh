341b8bace045618f916a8559b10b5ecf8f91abb9
guolidong <guolidong@genomics.cn>
2018-02-09 10:10:03 +0800

    add mergeClusterResult

--- build.sh
+++ build.sh
@@ -1,15 +1,27 @@
 #!/usr/bin/env bash
-cd src ; make ; cd -;
 
-mv ./src/BarcodeOnContig ./bin/
-mv ./src/BarcodeOnBin ./bin/
-mv ./src/ClusterGap ./bin/
-mv ./src/FormatBarcodeOnRef ./bin/
-mv ./src/UnicomGraph ./bin/
-mv ./src/BinCluster ./bin/
-mv ./src/ContigTypeByRef ./bin/
-mv ./src/StaticsticUnique ./bin/
-mv ./src/LinearClusterResult ./bin/
-mv ./src/Sam2ReadInContig ./bin/
-mv ./src/test ./bin/
-mv ./src/ContigGraphType ./bin/
+if [[ $# == 0 ]] ; then
+
+    cd src ; make ; cd -;
+
+    mv ./src/BarcodeOnContig ./bin/
+    mv ./src/BarcodeOnBin ./bin/
+    mv ./src/ClusterGap ./bin/
+    mv ./src/FormatBarcodeOnRef ./bin/
+    mv ./src/UnicomGraph ./bin/
+    mv ./src/BinCluster ./bin/
+    mv ./src/ContigTypeByRef ./bin/
+    mv ./src/StaticsticUnique ./bin/
+    mv ./src/LinearClusterResult ./bin/
+    mv ./src/Sam2ReadInContig ./bin/
+    mv ./src/test ./bin/
+    mv ./src/ContigGraphType ./bin/
+    mv ./src/MergeClusterResult ./bin/
+else
+    cd src ; make $@ ; cd -
+    while [[ $# -gt 0 ]]
+    do
+        mv ./src/$1 ./bin/
+        shift
+    done
+fi
