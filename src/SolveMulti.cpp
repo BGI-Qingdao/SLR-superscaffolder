dbbfefb40f8f941ce244a27436a660b1a512b247
guolidong <guolidong@genomics.cn>
2018-04-17 09:21:06 +0800

    save

--- src/SolveMulti.cpp
+++ src/SolveMulti.cpp
@@ -37,13 +37,14 @@ int main(int argc , char ** argv)
 {
 
     START_PARSE_ARGS
-        DEFINE_ARG_DETAIL(int , type, 'o',false,"1 for len , 2 for simularity");
+        DEFINE_ARG_DETAIL(int , type, 'o',false,"1 for len , 2 for simularity \
+                                ,3 for split multi and base \
+                                ,4 for solve multi");
         DEFINE_ARG_DETAIL(int , len_max, 'a',false,"max_len for first close");
         DEFINE_ARG_DETAIL(int , len_min, 'i',false,"min len for second close");
         DEFINE_ARG_DETAIL(float, sim, 's',false,"min simularity");
         DEFINE_ARG_DETAIL(int, index, 'd',false,"index");
     END_PARSE_ARGS
-
 
     std::string line;
     int index1 = 0;
