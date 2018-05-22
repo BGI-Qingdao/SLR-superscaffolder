0bce1d0ccba3a67e8fc1a3138a061bba4aaa81e6
guolidong <guolidong@genomics.cn>
2018-05-22 11:30:13 +0800

    refactor StaticsticUnique

--- src/StaticsticUnique.cpp
+++ src/StaticsticUnique.cpp
@@ -1,62 +1,97 @@
-#include "contig_barcode.h"
-#include "argsparser.h"
+#include "common/args/argsparser.h"
 #include "common/log/log.h"
 #include "common/log/logfilter.h"
+#include "soap2/soap2.h"
 #include <iostream>
+#include <map>
 
-using namespace BGIQD::JOB01;
-using namespace BGIQD::ARGS;
+struct AppConfig
+{
+    struct ContigInfo
+    {
+        BGIQD::SOAP2::ContigId id ;
+        int length;
+        float cov ;
+        int  tip ;
+    };
 
+    int K;
+    int min;
+    long total ;
+    float cov_all ;
+    std::map<BGIQD::SOAP2::ContigId , ContigInfo> contigInfo;
+    BGIQD::LOG::logger log;
+    float Ecov ;
+    float UniqueLow ;
+    float UniqueHigh  ;
+
+    void Init(int k, int m)
+    {
+        K = k ;
+        min = m;
+        total = 0;
+        cov_all = 0 ;
+        Ecov = 0;
+        UniqueLow = 0 ;
+        UniqueHigh = 0 ;
+        contigInfo.clear();
+        BGIQD::LOG::logfilter::singleton().get("StatictisUnique",BGIQD::LOG::loglevel::INFO,log);
+    }
+
+    void LoadContigInfio(std::istream & ist)
+    {
+        std::string line ;
+        while(!std::getline(ist,line).eof())
+        {
+            ContigInfo data;
+            sscanf(line.c_str() ,">%u length %d cvg_%f_tip_%d",&data.id , &data.length , &data.cov , &data.tip);
+            if( data.length > (2*K)+1 && data.cov > 10 )
+            {
+                total += data.length;
+                cov_all += (data.length * data.cov);
+            }
+            contigInfo[data.id] = data;
+        }
+    }
+
+    void CalcCov()
+    {
+        Ecov = cov_all/(float) total;
+        UniqueLow = 0.5f*Ecov;
+        UniqueHigh = 1.5f*Ecov ;
+        log<<BGIQD::LOG::lstart()<<"E(cov) = "<< Ecov<< " range ( "<<UniqueLow<<" , "<<UniqueHigh<<")"<< BGIQD::LOG::lend();
+    }
+
+    void PrintUniqueInfo(std::ostream & ost)
+    {
+        long index = 0 ;
+        long index1 = 0 ;
+        for( const auto &i : contigInfo)
+        {
+            auto & contig = i.second ;
+            index1 ++ ;
+            if(contig.cov > UniqueLow && contig.cov < UniqueHigh && contig.length >=min )
+            {
+                index ++ ;
+                ost<<contig.id<<'\t'<<contig.length<<std::endl;
+            }
+        }
+        log<<BGIQD::LOG::lstart()<<index << " seed in "<<index1<<" contigs"<< BGIQD::LOG::lend();
+    }
+
+}config;
 
 int main(int argc , char ** argv)
 {
     START_PARSE_ARGS
-    DEFINE_ARG_DETAIL(int , kvalue, 'K',false,"K value");
-    DEFINE_ARG_DETAIL(int , min, 'm',false,"min length of contig");
+    DEFINE_ARG_REQUIRED(int ,kvalue,"K value");
+    DEFINE_ARG_REQUIRED(int ,min ," bin size ");
     END_PARSE_ARGS
 
-    BGIQD::LOG::logger log;
-    BGIQD::LOG::logfilter::singleton().get("StatictisUnique",BGIQD::LOG::loglevel::INFO,log);
-
-    std::string line ;
-    std::map<int , std::tuple<int , float , int > > contigs;
-
-    long total = 0;
-    double cov_all = 0.0f ;
-
-    while(!std::getline(std::cin,line).eof())
-    {
-        int contigId , length;
-        float cov ;
-        int tip ;
-        sscanf(line.c_str() ,">%d length %d cvg_%f_tip_%d",&contigId , &length , &cov , &tip);
-        if( length > (2*kvalue.to_int())+1 && cov > 10 )
-        {
-            total += length;
-            cov_all += (length * cov);
-        }
-        contigs[contigId] = std::make_tuple( length , cov , tip );
-    }
-    float Ecov = cov_all/(float) total;
-    float UniqueLow = 0.5f*Ecov;
-    float UniqueHigh = 1.5f*Ecov ;
-
-    log<<BGIQD::LOG::lstart()<<"E(cov) = "<< Ecov<< " range ( "<<UniqueLow<<" , "<<UniqueHigh<<")"<< BGIQD::LOG::lend();
-    long index = 0 ;
-    long index1 = 0 ;
-    for( const auto &  contig : contigs)
-    {
-        index1 ++ ;
-        float cov = std::get<1>(contig.second);
-        int len = std::get<0>(contig.second);
-        if(cov > UniqueLow && cov < UniqueHigh && len >=min.to_int() )
-        {
-            index ++ ;
-            std::cout<<contig.first<<'\t'<<len<<std::endl;
-        }
-    }
-
-    log<<BGIQD::LOG::lstart()<<index << " seed in "<<index1<<" contigs"<< BGIQD::LOG::lend();
+    config.Init(kvalue.to_int(),min.to_int());
+    config.LoadContigInfio(std::cin);
+    config.CalcCov();
+    config.PrintUniqueInfo(std::cout);
 
     return 0;
 }
