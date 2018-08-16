754c7b7324d0838cf54e517859f32896f679b655
guolidong <guolidong@genomics.cn>
2018-08-16 16:33:33 +0800

    distribution

---
+++ src/algorithm/distribution/distribution.h
@@ -0,0 +1,65 @@
+#ifndef __ALGORITHM_DISTRIBUTION_DISTRIBUTION_H__
+#define __ALGORITHM_DISTRIBUTION_DISTRIBUTION_H__
+#include <map>
+#include "algorithm/interval/Interval.h"
+namespace BGIQD {
+    namespace DISTRIBUTION {
+
+        template<class T>
+            struct IntervalDistribution
+            {
+                typedef BGIQD::INTERVAL::Interval<T>  Item;
+                std::map<Item, int> freqs;
+                std::map<Item, float> percents;
+
+                void Init(const T & bin ,const  T &  min ,const T & max )
+                {
+                    for( T m = min ; m < max ; m += bin )
+                    {
+                        freqs[Item(m,m+bin)] = 0 ;
+                    }
+                }
+
+                void Count(const T & t )
+                {
+                    for( auto & pair : freqs )
+                    {
+                        if ( pair.first.IsContain(t) )
+                        {
+                            pair.second ++ ;
+                            return ;
+                        }
+                    }
+                }
+
+                void CalcPercent()
+                {
+                    int total = 0 ;
+                    for( auto & pair : freqs )
+                    {
+                        total += pair.second ;
+                    }
+
+                    for( auto & pair : freqs )
+                    {
+                        percents[pair.first] = (float)pair.second  / (float)total;
+                    }
+                }
+
+                float GetPercent(const T & t)
+                {
+                    for( auto & pair : percents)
+                    {
+                        if ( pair.first.IsContain(t) )
+                        {
+                            return pair.second ;
+                        }
+                    }
+                    return 0.0f ;
+                }
+            };
+    }
+
+}
+
+#endif //__ALGORITHM_DISTRIBUTION_DISTRIBUTION_H__
---
+++ src/algorithm/distribution/distribution_test.cpp
@@ -0,0 +1,25 @@
+#include "algorithm/distribution/distribution.h"
+#include "common/test/Test.h"
+
+TEST_MODULE_INIT(distribution)
+
+TEST(DD)
+{
+    typedef BGIQD::DISTRIBUTION::IntervalDistribution<int> DD;
+    DD d;
+    d.Init(10,0,30);
+    d.Count(0);
+    d.Count(1);
+    d.Count(2);
+    d.Count(3);
+    d.Count(13);
+    d.Count(15);
+    d.Count(23);
+    d.Count(33);
+    d.CalcPercent();
+    CHECK_FLOAT(float(4)/float(7) ,d.GetPercent(5)); 
+    CHECK_FLOAT(float(2)/float(7) ,d.GetPercent(15)); 
+    CHECK_FLOAT(float(1)/float(7) ,d.GetPercent(25)); 
+    CHECK_FLOAT(float(0)/float(7) ,d.GetPercent(35)); 
+}
+
