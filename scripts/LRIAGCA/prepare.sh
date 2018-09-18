f86dbf83431fdd91599e371fb302b3579bd647e2
guolidong <guolidong@genomics.cn>
2018-09-18 16:31:43 +0800

    save

--- scripts/LRIAGCA/prepare.sh
+++ scripts/LRIAGCA/prepare.sh
@@ -1,50 +1,50 @@
 #!/bin/bash
-source conf.ini
-## check parameters
-#if [[ $# != 1 ]] ; then
-#    echo "Usage : $0 conf_file"
-#    exit 1
-#fi
-#
-## check parameters as file
-#if [[  -f "$1" ]] ; then 
-#    source "$1"
-#else
-#    echo "file $1 is not exsist !!! exit ... "
-#    exit 1
-#fi
-#
-## check SOAP diectory
-#Contig=$SOAP/$PREFIX".contig"
-#Edge=$SOAP/$PREFIX".updated.edge"
-#Arc=$SOAP/$PREFIX".Arc"
-#Index=$SOAP/$PREFIX".ContigIndex"
-#if [[ ! -e $Contig ||  ! -e $Edge || ! -e $Arc || ! -e $Index ]] ; then 
-#    echo "$SOAP/$PREFIX is not a valid SOAP prefix . exit ..."
-#    exit 1
-#fi
-#
-## check target directory
-#if [[ -d $PROJECT_NAME ]] ; then 
-#    echo "Project $PROJECT_NAME already exsist !!! exit ..."
-#    echo "you can enter directory $PROJECT_NAME and run commands directly ."
-#    exit 1
-#fi
-#
-## mkdir target file
-#echo "mkdir $PROJECT_NAME"
-#mkdir $PROJECT_NAME
-## ln input file
-#cd $PROJECT_NAME
-#echo "ln -s $Contig"
-#ln -s $Contig
-#echo "ln -s $Edge"
-#ln -s $Edge
-#echo "ln -s $Index"
-#ln -s $Index
-#echo "ln -s $Arc"
-#ln -s $Arc
-#
+#source conf.ini
+# check parameters
+if [[ $# != 1 ]] ; then
+    echo "Usage : $0 conf_file"
+    exit 1
+fi
+
+# check parameters as file
+if [[  -f "$1" ]] ; then 
+    source "$1"
+else
+    echo "file $1 is not exsist !!! exit ... "
+    exit 1
+fi
+
+# check SOAP diectory
+Contig=$SOAP/$PREFIX".contig"
+Edge=$SOAP/$PREFIX".updated.edge"
+Arc=$SOAP/$PREFIX".Arc"
+Index=$SOAP/$PREFIX".ContigIndex"
+if [[ ! -e $Contig ||  ! -e $Edge || ! -e $Arc || ! -e $Index ]] ; then 
+    echo "$SOAP/$PREFIX is not a valid SOAP prefix . exit ..."
+    exit 1
+fi
+
+# check target directory
+if [[ -d $PROJECT_NAME ]] ; then 
+    echo "Project $PROJECT_NAME already exsist !!! exit ..."
+    echo "you can enter directory $PROJECT_NAME and run commands directly ."
+    exit 1
+fi
+
+# mkdir target file
+echo "mkdir $PROJECT_NAME"
+mkdir $PROJECT_NAME
+# ln input file
+cd $PROJECT_NAME
+echo "ln -s $Contig"
+ln -s $Contig
+echo "ln -s $Edge"
+ln -s $Edge
+echo "ln -s $Index"
+ln -s $Index
+echo "ln -s $Arc"
+ln -s $Arc
+
 # prepare scripts
 
 MACRO=" -D xxx=$PREFIX \
@@ -71,6 +71,4 @@ m4 $MACRO $DATA/run.m4 >run.sh
 
 chmod u+x step_*.sh 
 chmod u+x run.sh
-
-
 
