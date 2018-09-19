248b3b3e4a68ca4db25c5cf717f9f667e5fe8304
guolidong <guolidong@genomics.cn>
2018-09-19 15:57:57 +0800

    pipeline script save

--- scripts/LRIAGCA/prepare.sh
+++ scripts/LRIAGCA/prepare.sh
@@ -1,51 +1,53 @@
 #!/bin/bash
-#source conf.ini
-# check parameters
-if [[ $# != 1 ]] ; then
-    echo "Usage : $0 conf_file"
-    exit 1
-fi
-
-# check parameters as file
-if [[  -f "$1" ]] ; then 
-    source "$1"
-else
-    echo "file $1 is not exsist !!! exit ... "
-    exit 1
-fi
-
-# check SOAP diectory
-SOAP=$SOAP_DIR
-Contig=$SOAP/$PREFIX".contig"
-Edge=$SOAP/$PREFIX".updated.edge"
-Arc=$SOAP/$PREFIX".Arc"
-Index=$SOAP/$PREFIX".ContigIndex"
-if [[ ! -e $Contig ||  ! -e $Edge || ! -e $Arc || ! -e $Index ]] ; then 
-    echo "$SOAP/$PREFIX is not a valid SOAP prefix . exit ..."
-    exit 1
-fi
-
-# check target directory
-if [[ -d $PROJECT_NAME ]] ; then 
-    echo "Project $PROJECT_NAME already exsist !!! exit ..."
-    echo "you can enter directory $PROJECT_NAME and run commands directly ."
-    exit 1
-fi
-
-# mkdir target file
-echo "mkdir $PROJECT_NAME"
-mkdir $PROJECT_NAME
-# ln input file
-cd $PROJECT_NAME
-echo "ln -s $Contig"
-ln -s $Contig
-echo "ln -s $Edge"
-ln -s $Edge
-echo "ln -s $Index"
-ln -s $Index
-echo "ln -s $Arc"
-ln -s $Arc
-# prepare scripts
+source conf.ini
+## check parameters
+#if [[ $# != 1 ]] ; then
+#    echo "Usage : $0 conf_file"
+#    exit 1
+#fi
+#
+## check parameters as file
+#if [[  -f "$1" ]] ; then 
+#    source "$1"
+#else
+#    echo "file $1 is not exsist !!! exit ... "
+#    exit 1
+#fi
+#
+## check SOAP diectory
+#SOAP=$SOAP_DIR
+#Contig=$SOAP/$PREFIX".contig"
+#Edge=$SOAP/$PREFIX".updated.edge"
+#Arc=$SOAP/$PREFIX".Arc"
+#Index=$SOAP/$PREFIX".ContigIndex"
+#if [[ ! -e $Contig ||  ! -e $Edge || ! -e $Arc || ! -e $Index ]] ; then 
+#    echo "$SOAP/$PREFIX is not a valid SOAP prefix . exit ..."
+#    exit 1
+#fi
+#
+## check target directory
+#if [[ -d $PROJECT_NAME ]] ; then 
+#    echo "Project $PROJECT_NAME already exsist !!! exit ..."
+#    echo "you can enter directory $PROJECT_NAME and run commands directly ."
+#    exit 1
+#fi
+#
+## mkdir target file
+#echo "mkdir $PROJECT_NAME"
+#mkdir $PROJECT_NAME
+## ln input file
+#cd $PROJECT_NAME
+#echo "ln -s $Contig"
+#ln -s $Contig
+#echo "ln -s $Edge"
+#ln -s $Edge
+#echo "ln -s $Index"
+#ln -s $Index
+#echo "ln -s $Arc"
+#ln -s $Arc
+#chmod a-w $PREFIX*
+#chmod a-x $PREFIX*
+## prepare scripts
 
 MACRO=" -D xxx=$PREFIX \
    -D BIN=$EXEC_DIR \
@@ -61,6 +63,8 @@ MACRO=" -D xxx=$PREFIX \
    -D TAIL=$TAIL_DEL \
    -D LF=$LEN_FACTOR "
 
+echo "Generate all scripts ..."
+#cp $DATA/__common_function.sh ./
 m4 $MACRO $DATA/step_0_prepare_info.m4 >step_0_prepare_info.sh
 m4 $MACRO $DATA/step_1_calc_seeds.m4 >step_1_calc_seeds.sh
 m4 $MACRO $DATA/step_2_bin_cluster.m4 >step_2_bin_cluster.sh
@@ -69,7 +73,7 @@ m4 $MACRO $DATA/step_4_contig_road.m4 >step_4_contig_road.sh
 m4 $MACRO $DATA/step_5_contig_road_fill.m4 >step_5_contig_road_fill.sh
 m4 $MACRO $DATA/step_6_merge_contig.m4 >step_6_merge_contig.sh
 m4 $MACRO $DATA/run.m4 >run.sh
-
 chmod u+x step_*.sh 
 chmod u+x run.sh
 
+echo "prepare.sh done"
--- scripts/LRIAGCA/step_0_prepare_info.m4
+++ scripts/LRIAGCA/step_0_prepare_info.m4
@@ -1,9 +1,19 @@
 #!/bin/bash
 
-BWA index xxx.contig >log_bwa_index 2>&1
+source ./__common_function.sh || exit 1
 
-BWA mem -t THREADS -k eval(TAIL+1) xxx.contig R1 R2 >xxx.read2contig.sam 2>>log_bwa_mem
+check_input xxx.contig R1 R2
 
-BIN/Sam2ReadOnContig --prefix xxx 2>>log_sam2readoncontig
+BWA index xxx.contig >log_bwa_index 2>&1 || exit 1
 
-BIN/SplitInfo --prefix xxx --parse_barcode 2>>log_splitinfo
+try_backup_list xxx.read2contig.sam log_bwa_mem
+BWA mem -t THREADS -k eval(TAIL+1) xxx.contig R1 R2 >xxx.read2contig.sam 2>>log_bwa_mem ||exit 1
+check_output xxx.read2contig.sam log_bwa_mem
+
+try_backup_list xxx.read2contig
+BIN/Sam2ReadOnContig --prefix xxx 2>>log_sam2readoncontig || exit 1
+check_output xxx.read2contig
+
+try_backup_list xxx.barcodeOnContig xxx.contigOnBarcode xxx.pe_info xxx.pe_pairs
+BIN/SplitInfo --prefix xxx --parse_barcode 2>>log_splitinfo || exit 1
+check_output xxx.barcodeOnContig xxx.contigOnBarcode
